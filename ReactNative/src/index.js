961ccc90c0f16e9a1595fa68f487be7bc36686a1
Pawel Buderaski <pbuderaski@gmail.com>
2018-04-03 14:46:23 +0100

    chore: added linting settings

--- ReactNative/src/index.js
+++ ReactNative/src/index.js
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   Platform,
@@ -19,7 +13,7 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-export default class App extends Component<{}> {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
