06fd3207d37ecb8a3f68b6a5d6917cc0b7621fa2
Avi Levin <aviavi23@gmail.com>
2019-07-03 17:16:07 +0300

    feature(iOSGeneralPlugin): add hook implementation to the iOS general sample project

--- GeneralPlugin/iOS/SampleGeneralPlugin/SampleGeneralPlugin.swift
+++ GeneralPlugin/iOS/SampleGeneralPlugin/SampleGeneralPlugin.swift
@@ -10,8 +10,9 @@ import Foundation
 import ZappPlugins
 import ZappGeneralPluginsSDK
 
-@objc public class SampleGeneralPlugin: ZPGeneralBaseProvider {
+@objc public class SampleGeneralPlugin: ZPGeneralBaseProvider, ZPAppLoadingHookProtocol {
     
+    // MARK: - ZPAdapterProtocol implementation
     /**
         Implement this method if you need the plugin to respond to a URL scheme.
         The parameteters of the URL scheme will be passed as a dictionary to be handled by the plugin.
@@ -28,5 +29,62 @@ import ZappGeneralPluginsSDK
      */
     @objc public func handleUrlScheme(_ params: NSDictionary) {
         
+    }
+    
+    // MARK: - ZPAdapterProtocol implementation
+    /*
+     This protocol should be implemented by plugin that need to add some logic before application load data and before rootViewController presented. example: login plugin will implement this inteface in order to make login flow before application data launched.
+     
+     All the methods in this protocol are optional, implement only the necessary ones.
+     */
+    
+    /*
+     This method called after Plugins loaded locally, but the account load failed
+     */
+    @objc public func executeOnFailedLoading(completion: (() -> Void)?) {
+        //Write you implementation here
+        
+        //Release the hook
+        completion?()
+    }
+    
+    /*
+     This method called after Plugins loaded, and also after initial account data retrieved, you can add logic that not related to the application data.
+     */
+    @objc public func executeOnLaunch(completion: (() -> Void)?) {
+        //Write you implementation here
+        
+        //Release the hook
+        completion?()
+    }
+    
+    /*
+     This method called after all the data loaded and before viewController presented.
+     */
+    @objc public func executeOnApplicationReady(displayViewController: UIViewController?, completion: (() -> Void)?) {
+        //Write you implementation here
+        
+        //Release the hook
+        completion?()
+    }
+    
+    /*
+     This method called after viewController is presented.
+     */
+    @objc public func executeAfterAppRootPresentation(displayViewController: UIViewController?, completion: (() -> Void)?) {
+        //Write you implementation here
+        
+        //Release the hook
+        completion?()
+    }
+    
+    /*
+     This method called when the application:continueUserActivity:restorationHandler is called.
+     */
+    @objc public func executeOnContinuingUserActivity(_ userActivity: NSUserActivity?, completion: (() -> Void)?) {
+        //Write you implementation here
+        
+        //Release the hook
+        completion?()
     }
 }
